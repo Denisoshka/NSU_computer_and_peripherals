902074267a0b576a4e5ca8c843edc983c6a64161
denis <d.zhdanov@g.nsu.ru>
2023-09-22 19:46:16 +0700

    refactor sin_x

--- lab1/c/main.c
+++ lab1/c/main.c
@@ -3,19 +3,7 @@
 #include <sys/times.h>// for times
 #include <unistd.h>   // for sysconf
 
-long double sin_x(const double x, const size_t n) {
-  long double rez = x;
-  long double sign;
-  long double prev_rez = x;
-
-  for (int i = 3, sign = -1; i < n; i += 2, sign *= -1) {
-    prev_rez = (sign * rez * prev_rez * prev_rez) / ((i - 1) * i);
-    rez += prev_rez;
-  }
-  return rez;
-}
-
-long double sin_e2(double x, long long int n) {
+long double sin_x(double x, long long int n) {
   long double start_sum = x;
   long double sum = 0.0;
 //  int i = 1;
@@ -36,7 +24,7 @@ int main(int argc, char** argv) {
   long clocks_per_sec = sysconf(_SC_CLK_TCK);
   long clocks;
   times(&start);
-  double rez = sin_e2(x, n);
+  double rez = sin_x(x, n);
   times(&end);
   clocks = end.tms_utime - start.tms_utime;
   printf("Time taken: %lf sec.\n", (double) clocks / clocks_per_sec);
